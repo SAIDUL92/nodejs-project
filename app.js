ae21e9b8edf5885a52805a65ff6896d4ab215f5f
MOHAMMAD SAIDUL ISLAM <isaidul92@gmail.com>
2023-06-01 17:07:02 +0600

    parsed data with row nodejs
    parsed data with row nodejs

--- app.js
+++ app.js
@@ -1,19 +1,43 @@
 const http = require('http');
-const { text } = require('stream/consumers');
+const fs = require('fs');
+const { buffer } = require('stream/consumers');
 const server = http.createServer((req, res) => {
 
     const url = req.url;
+    const method = req.method;
 
     if (url === '/') {
         res.write('<html>')
         res.write('<head><title>Enter your message</title></head>')
         res.write('<body>')
-        res.write('<form action="/message" method="post"><input type="text" name="name"/> <button type="Submit">submit</button> </form>')
+        res.write('<form action="/message" method="post"><input type="text" name="message"/> <button type="Submit">submit</button> </form>')
         res.write('</body>')
         res.write('</html>')
         return res.end()
 
     }
+
+
+    if (url === '/message' && method === 'POST') {
+        const data = []
+
+        req.on('data', (chank) => {
+            data.push(chank)
+        })
+
+        req.on('end', (test) => {
+            const parsedData = Buffer.concat(data).toString();
+            const message = parsedData.split('=')[1];
+            console.log(message);
+
+            // fs.writeFileSync('message.txt', message);
+        })
+        res.statusCode = 302;
+        res.setHeader("Location", '/');
+        return res.end()
+    }
+
+
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>')
     res.write('<head><title> my title </title></head>')
