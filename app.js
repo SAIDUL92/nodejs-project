67d911174ad735502331db7b99d6dafa224c6668
MOHAMMAD SAIDUL ISLAM <isaidul92@gmail.com>
2023-06-05 12:34:36 +0600

    ued app for server listening
    ued app for server listening

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
-const http = require('http');
+
 const express = require('express');
 const app = express()
 
-
-// const routes = require('./routes')
-// const server = http.createServer(routes)
 
 app.use((req, res, next) => {
 
@@ -17,10 +14,10 @@ app.use((req, res, next) => {
 app.use((req, res, next) => {
 
     console.log('Second middleware');
+    
+    res.send('<h1>Hello from node js!</h1>')
 
 })
 
 
-const server = http.createServer(app)
-
-server.listen(3000)
+app.listen(3000)
