08525292a7939e4a291bcb61d4785ec055072748
MOHAMMAD SAIDUL ISLAM <isaidul92@gmail.com>
2023-06-05 14:32:36 +0600

    added multiple routes
    added multiple routes

--- app.js
+++ app.js
@@ -3,19 +3,16 @@ const express = require('express');
 const app = express()
 
 
-app.use((req, res, next) => {
+app.use('/products', (req, res, next) => {
 
-    console.log('First middleware');
-    next()
+    res.send('<h1>Hello from node js products page</h1>')
 
 })
 
 
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
 
-    console.log('Second middleware');
-    
-    res.send('<h1>Hello from node js!</h1>')
+    res.send('<h1>Hello from node js home page</h1>')
 
 })
 
