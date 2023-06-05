773c52a31a695168b687a0b4dd82340d5caea567
MOHAMMAD SAIDUL ISLAM <isaidul92@gmail.com>
2023-06-05 12:22:43 +0600

    Middleware added
    Middleware added

--- app.js
+++ app.js
@@ -5,6 +5,22 @@ const app = express()
 
 // const routes = require('./routes')
 // const server = http.createServer(routes)
+
+app.use((req, res, next) => {
+
+    console.log('First middleware');
+    next()
+
+})
+
+
+app.use((req, res, next) => {
+
+    console.log('Second middleware');
+
+})
+
+
 const server = http.createServer(app)
 
 server.listen(3000)
