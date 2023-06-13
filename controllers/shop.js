a0b25da3bd34e2abd0b42a828a2e8758c2e68e08
MOHAMMAD SAIDUL ISLAM <isaidul92@gmail.com>
2023-06-13 14:05:27 +0600

    Cart Model
    Cart Model added

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
@@ -45,7 +46,10 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const productId = req.body.productId;
-  console.log(productId);
+  Product.findByid(productId, (product) => {
+    Cart.addProduct(productId, product.price)
+    console.log(productId, product.price);
+  })
   res.redirect('/cart');
 }
 
---
+++ models/cart.js
@@ -0,0 +1,43 @@
+const fs = require('fs')
+const path = require('path')
+
+const filesPath = path.join(path.dirname(require.main.filename), "data", "cart.json"
+);
+
+module.exports = class cart {
+    static addProduct(id, productPrice) {
+        // fetch the previuos cart
+        fs.readFile(filesPath, (err, content) => {
+            let cart = { products: [], totalPrice: 0 }
+            if (!err) {
+
+                cart = JSON.parse(content)
+            }
+            // analyz the cart => find existing product
+
+            const existingProductindex = cart.products.findIndex(prod => prod.id === id);
+            const existingProduct = cart.products[existingProductindex]
+            let updatedProduct;
+            // add new product / increase 
+            if (existingProduct) {
+                updatedProduct = { ...existingProduct };
+                updatedProduct.qty = updatedProduct.qty + 1;
+                cart.products = [...cart.products];
+                cart.products[existingProductindex] = updatedProduct
+            }
+
+            else {
+                updatedProduct = { id: id, qty: 1 }
+                cart.products = [...cart.products, updatedProduct]
+            }
+
+            cart.totalPrice = cart.totalPrice + +productPrice;
+
+            fs.writeFile(filesPath, JSON.stringify(cart), err => {
+                console.log(err);
+            })
+        })
+
+
+    }
+}
