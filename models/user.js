0173f68ae9a4b9e81540810b3588ac56387c3876
MOHAMMAD SAIDUL ISLAM <isaidul92@gmail.com>
2023-06-22 13:58:30 +0600

    Working on Cart Items & Orders
    Working on Cart Items & Orders

--- models/user.js
+++ models/user.js
@@ -4,16 +4,30 @@ const getDb = require("../util/database").getDb;
 const ObjectId = mongodb.ObjectId;
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart) {
     this.username = username;
     this.email = email;
+    this.cart = cart;
   }
 
   save() {
     const db = getDb();
     return db.collection("users").insertOne(this).then().catch();
   }
-  
+
+
+  adtoCart(products) {
+    const cartProduct = this.cart.items.findindex(product => product._id === products._id);
+
+    const updatedCart = { items: [{ ...products, quantity: 1 }] };
+
+    const db = getDb();
+
+    return db.collection('users').updateOne({ _id: new ObjectId(this._id) }, { $set: { cart: updatedCart } }).then(result => { console.log(result); }).catch(err => { console.log(err); })
+
+  }
+
+
   static findById(id) {
     const db = getDb();
     return db
