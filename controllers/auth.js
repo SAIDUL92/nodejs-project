a2183a797130532fb65532188b99b02a4511da23
MOHAMMAD SAIDUL ISLAM <isaidul92@gmail.com>
2023-06-26 11:03:35 +0600

    Deleting a Cookie
    Deleting a Cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,29 @@
+const User = require("../models/user");
+
 exports.getLogin = (req, res, next) => {
   // const isLoggedIn = req.get("Cookie").split(";")[1].trim().split("=")[1] === 'true';
   console.log(req.session.isLoggedIn);
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    isAuthenticated:false,
+    isAuthenticated: false,
   });
 };
+
 exports.postLogin = (req, res, next) => {
-  req.session.isLoggedIn = true;
-  res.redirect("/");
+  User.findById("6495d9398baf80a1e0274ec8")
+    .then((user) => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      res.redirect("/");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+exports.postLogOut = (req, res, next) => {
+  req.session.destroy((err) => {
+    console.log(err);
+    res.redirect("/");
+  });
 };
